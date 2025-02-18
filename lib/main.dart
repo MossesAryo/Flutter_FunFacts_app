20e7656eb4f9083c4d48c090f28b945b7a76544c
MossesAryo <mossesaryobimo@gmail.com>
2025-02-18 19:09:21 +0700

    Layouting and adding settings screen

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MainScreen();
+    return MaterialApp(
+        theme: ThemeData.light(useMaterial3: true),
+        home: MainScreen());
   }
 }
