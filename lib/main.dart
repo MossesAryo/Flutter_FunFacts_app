2fdef8358258b30415123cffd9b4fccc400e56a2
MossesAryo <mossesaryobimo@gmail.com>
2025-02-19 10:11:02 +0700

    Working on theme(Add logic)#4

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:funfacts/providers/themeProvider.dart';
 import 'package:funfacts/screens/main_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => ThemeProvider(), child: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -16,7 +18,12 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    var themeProvider = Provider.of<ThemeProvider>(context);
+
     return MaterialApp(
-        theme: ThemeData.light(useMaterial3: true), home: MainScreen());
+        theme: themeProvider.isDarkModeChecked
+            ? ThemeData.dark(useMaterial3: true)
+            : ThemeData.light(useMaterial3: true),
+        home: MainScreen());
   }
 }
--- lib/providers/themeProvider.dart
+++ lib/providers/themeProvider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  bool isDarkModeChecked = false;
+  bool isDarkModeChecked = true;
 }
